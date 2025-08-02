bf192b5bef91d733baa810070553bc642928f206
AfshaanFathima <afshaanfathimask@gmail.com>
2025-08-03 01:11:08 +0530

    src

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+import { AppKitProvider } from './providers/AppKitProvider';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AppKitProvider>
+      <App />
+    </AppKitProvider>
+  </React.StrictMode>
+);
